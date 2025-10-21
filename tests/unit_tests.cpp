c31fbe2adf818649a3dbb1322d1a373d35a872d8
olesiaBokhonok <olesia.bokhonok.ki.2022@lpnu.ua>
2025-10-21 21:07:07 +0300

    Update unit_tests.cpp

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -1,30 +1,14 @@
-// unit_tests.cpp
-// Simple, self-contained unit tests for math_operations::add
-//
-// Build:
-//   g++ -std=c++11 unit_tests.cpp math_operations.cpp -o unit_tests
-// Run:
-//   ./unit_tests
-//
-// The tests use assert and will abort on failure. Successful run prints a confirmation.
+#include <gtest/gtest.h>
+#include "../math_operations.h"
 
-#include <iostream>
-#include <cassert>
-#include "math_operations.h"
+TEST(BasicAddition, HandlesPositiveNumbers) {
+    EXPECT_EQ(add(3, 3), 6);
+}
 
-int main() {
-    using math_ops::add;
+TEST(BasicAddition, HandlesNegativeAndPositive) {
+    EXPECT_EQ(add(-2, 2), 0);
+}
 
-    // Basic cases
-    assert(add(0, 0) == 0);
-    assert(add(2, 3) == 5);
-    assert(add(-1, 1) == 0);
-    assert(add(-5, -7) == -12);
-
-    // Edge-ish cases
-    assert(add(2147483647, 0) == 2147483647); // INT_MAX + 0
-    assert(add(0, -2147483648) == -2147483648); // INT_MIN + 0
-
-    std::cout << "All unit tests passed.\n";
-    return 0;
+TEST(BasicAddition, HandlesZeros) {
+    EXPECT_EQ(add(0, 0), 0);
 }
