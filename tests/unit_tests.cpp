7090b17f9937da3b88237847148a09ddbb0b2633
olesiaBokhonok <olesia.bokhonok.ki.2022@lpnu.ua>
2025-10-21 21:11:08 +0300

    Update unit_tests.cpp

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -2,13 +2,13 @@
 #include "../math_operations.h"
 
 TEST(BasicAddition, HandlesPositiveNumbers) {
-    EXPECT_EQ(math_ops(3, 3), 6);
+    EXPECT_EQ(math_ops::add(3, 3), 6);
 }
 
 TEST(BasicAddition, HandlesNegativeAndPositive) {
-    EXPECT_EQ(math_ops(-2, 2), 0);
+    EXPECT_EQ(math_ops::add(-2, 2), 0);
 }
 
 TEST(BasicAddition, HandlesZeros) {
-    EXPECT_EQ(math_ops(0, 0), 0);
+    EXPECT_EQ(math_ops::add(0, 0), 0);
 }
