ba277488c55fe706a2af393ec9b29bbdca0c22a1
olesiaBokhonok <olesia.bokhonok.ki.2022@lpnu.ua>
2025-10-21 21:09:05 +0300

    Replace 'add' with 'math_ops' in unit tests

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -2,13 +2,13 @@
 #include "../math_operations.h"
 
 TEST(BasicAddition, HandlesPositiveNumbers) {
-    EXPECT_EQ(add(3, 3), 6);
+    EXPECT_EQ(math_ops(3, 3), 6);
 }
 
 TEST(BasicAddition, HandlesNegativeAndPositive) {
-    EXPECT_EQ(add(-2, 2), 0);
+    EXPECT_EQ(math_ops(-2, 2), 0);
 }
 
 TEST(BasicAddition, HandlesZeros) {
-    EXPECT_EQ(add(0, 0), 0);
+    EXPECT_EQ(math_ops(0, 0), 0);
 }
